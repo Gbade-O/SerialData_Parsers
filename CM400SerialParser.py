9c593555d78f811180a6fd0ff55e01f742844404
Gbade-O <gogunwumi@umassd.edu>
2020-12-11 09:05:57 -0500

    minor update
    just removed a few comments

--- CM400SerialParser.py
+++ CM400SerialParser.py
@@ -84,49 +84,49 @@ for folder in folders :
         #df_summary = df_summary.astype(int)
         df_Final = df_Final.append(df_summary,ignore_index = True)
         
-        # ##Make plot(s) and save to Plot directory
-        # fig, ax1=  plt.subplots(figsize=(20, 10))
-        # fig.suptitle("CM"+ folder + " Brew file#" + Brew, fontsize = 18)
-        # nrows = len(df)
-        # time = np.linspace(0,nrows,num=nrows)
-        # ax1 = plt.subplot(3,1,1)
-        # ax1.plot(time,df["Tout"],label = 'OutletTemp')
-        # ax1.plot(time,df["Tboil"], label = 'Boiler Temp')
-        # ax1.plot(time,df["Tptc"], label = 'Warmplate Temp')
-        # ax1.plot(time,df["Pwm"], label = 'Pump PWM')
-        # ax1.plot(time,df["RecB"], label = 'Recipe Block')
-        # plt.yticks(np.arange(0,160,10))
-        # ax1.legend(loc =0)
-        # plt.xlabel('Time(s)', fontsize = 14)
-        # plt.ylabel("Temp + Pump Rates", fontsize = 14)
-        # plt.title(MainPath[-6:] + " Control Response", fontsize = 18)
+        ##Make plot(s) and save to Plot directory
+        fig, ax1=  plt.subplots(figsize=(20, 10))
+        fig.suptitle("CM"+ folder + " Brew file#" + Brew, fontsize = 18)
+        nrows = len(df)
+        time = np.linspace(0,nrows,num=nrows)
+        ax1 = plt.subplot(3,1,1)
+        ax1.plot(time,df["Tout"],label = 'OutletTemp')
+        ax1.plot(time,df["Tboil"], label = 'Boiler Temp')
+        ax1.plot(time,df["Tptc"], label = 'Warmplate Temp')
+        ax1.plot(time,df["Pwm"], label = 'Pump PWM')
+        ax1.plot(time,df["RecB"], label = 'Recipe Block')
+        plt.yticks(np.arange(0,160,10))
+        ax1.legend(loc =0)
+        plt.xlabel('Time(s)', fontsize = 14)
+        plt.ylabel("Temp + Pump Rates", fontsize = 14)
+        plt.title(MainPath[-6:] + " Control Response", fontsize = 18)
         
-        # ax1_2 = ax1.twinx() ##Plot on secondary axis
-        # ax1_2.plot(time,df["FRate"], '-k',label = 'Flow rate')
-        # ax1_2.legend(loc = 'upper right')
+        ax1_2 = ax1.twinx() ##Plot on secondary axis
+        ax1_2.plot(time,df["FRate"], '-k',label = 'Flow rate')
+        ax1_2.legend(loc = 'upper right')
        
-        # ax3 = plt.subplot(3,1,2)
+        ax3 = plt.subplot(3,1,2)
         
-        # ax3.plot(time,df["Boil"], label= 'Boiler ON')
-        # ax3.plot(time, df["Ptc"], label = 'PTC ON')
-        # ax3.plot(time,df["RecB"], label = 'Recipe Block')
-        # ax3.legend(loc = 0)
+        ax3.plot(time,df["Boil"], label= 'Boiler ON')
+        ax3.plot(time, df["Ptc"], label = 'PTC ON')
+        ax3.plot(time,df["RecB"], label = 'Recipe Block')
+        ax3.legend(loc = 0)
         
         
-        # ax2 = plt.subplot(3,1,3)
-        # ax2.plot(time,df["CBV"], label = 'Current Block Volume')
-        # ax2.plot(time,df["CTV"], label = 'Current Total Volume')
-        # ax2.plot(time,df["Vtal"],label = 'Recipe Total Volume')
-        # plt.yticks(np.arange(0,max(df["Vtal"])+100,100))
-        # plt.xlabel('Time(s)')
-        # plt.ylabel('Volume(mL)')
-        # plt.title('Brew Volumes')
-        # ax2.legend()
+        ax2 = plt.subplot(3,1,3)
+        ax2.plot(time,df["CBV"], label = 'Current Block Volume')
+        ax2.plot(time,df["CTV"], label = 'Current Total Volume')
+        ax2.plot(time,df["Vtal"],label = 'Recipe Total Volume')
+        plt.yticks(np.arange(0,max(df["Vtal"])+100,100))
+        plt.xlabel('Time(s)')
+        plt.ylabel('Volume(mL)')
+        plt.title('Brew Volumes')
+        ax2.legend()
         
-        # SaveName = file.split('.')[0]
-        # SavePath = os.path.join(MainPath,folder,"Plots",SaveName)
-        # plt.savefig(SavePath)
-        # plt.show()        
+        SaveName = file.split('.')[0]
+        SavePath = os.path.join(MainPath,folder,"Plots",SaveName)
+        plt.savefig(SavePath)
+        plt.show()        
                 
                         
                     
